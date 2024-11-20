e95ff571410b00c1c001a4f63af879bfb84e660c
nMckenryan <n.mckenryan@gmail.com>
2024-11-20 23:52:16 +1100

    Printed all pokemon names to a json file

--- borrius_pokedex_api/pokemon_scraper.py
+++ borrius_pokedex_api/pokemon_scraper.py
@@ -1,5 +1,6 @@
 import datetime
 from pathlib import Path
+from urllib import response
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -22,23 +23,75 @@ class BorriusPokedexHelpers:
 bph = BorriusPokedexHelpers()
 
 
+# Corrects name of pokemon so it can be successfully found in pokeapi
+def correct_pokemon_name(p):
+    pokemon = p.lower().replace(". ", "-").replace("'", "")
+    corrections = {
+        "dome fossil": "kabuto",
+        "helix fossil": "omanyte",
+        "claw fossil": "anorith",
+        "root fossil": "lileep",
+        "skull fossil": "cranidos",
+        "armor fossil": "shieldon",
+        "cover fossil": "tirtouga",
+        "plume fossil": "archen",
+        "jaw fossil": "tyrunt",
+        "sail fossil": "amaura",
+        "old amber": "aerodactyl",
+        "galarian slowpoke": "slowpoke-galar",
+        "galarian darmanitan": "darmanitan-galar-standard",
+        "galarian ": lambda x: x.replace("galarian ", "") + "-galar",
+        "hisuian ": lambda x: x.replace("hisuian ", "") + "-hisui",
+        "alolan ": lambda x: x.replace("alolan ", "") + "-alola",
+        "indeedee\u2642": "indeedee-male",
+        "indeedee\u2640": "indeedee-female",
+        "flabe\u0301be\u0301": "flabebe",
+        "flab\u00e9b\u00e9": "flabebe",
+        "nidoran\u2642": "nidoran-m",
+        "nidoran\u2640": "nidoran-f",
+        "basculin": "basculin-red-striped",
+        "enamorus": "enamorus-incarnate",
+        "morpeko": "morpeko-full-belly",
+        "eiscue": "eiscue-ice",
+        "minior": "minior-red-meteor",
+        "oricorio": "oricorio-baile",
+        "pumpkaboo": "pumpkaboo-average",
+        "gourgeist": "gourgeist-average",
+        "wormadam": "wormadam-plant",
+        "meowstic": "meowstic-male",
+        "wishiwashi": "wishiwashi-solo",
+        "lycanroc": "lycanroc-midday",
+        "darmanitan": "darmanitan-standard",
+        "deoxys": "deoxys-normal",
+        "shaymin": "shaymin-land",
+        "keldeo": "keldeo-ordinary",
+    }
+    for key, value in corrections.items():
+        if key in pokemon:
+            if callable(value):
+                return value(pokemon)
+            else:
+                return value
+    return pokemon
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+
+class PokemonNameScraper(scrapy.Spider):
+    name = "pokemon_name_scraper"
 
     def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
+        with open('items.json', 'w') as f:
+            f.truncate(0)
+        borrius_numbers = range(1, 3)
+                
+        for number in borrius_numbers:
+            url = f"https://www.pokemonunboundpokedex.com/borrius/{number}"
             yield scrapy.Request(url=url, callback=self.parse)
-
+        
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        name_tag = response.xpath('//h3[@class="card-title text-4xl"]/text()').get()
+        pokemon_name_sanitised = correct_pokemon_name(name_tag).replace("Name: ", "")
+        yield {"pokemon": pokemon_name_sanitised}
+
 
 process = CrawlerProcess(
     settings={
@@ -48,5 +101,5 @@ process = CrawlerProcess(
     }
 )
 
-process.crawl(QuotesSpider)
-process.start()  # the script will block here until the crawling is finished
+process.crawl(PokemonNameScraper)
+process.start()
